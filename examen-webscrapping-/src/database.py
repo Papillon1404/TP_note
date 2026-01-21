aec0ae8cf700203b2521853709f83a8f736cf775
Arthur Auillans <arthur.auillans@etu.minesparis.psl.eu>
2026-01-21 14:22:14 +0100

    ajout des f° createdb et insertline dans la class

--- examen-webscrapping-/src/database.py
+++ examen-webscrapping-/src/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
@@ -9,12 +10,31 @@ class DatabaseManager:
     def _init_(self) :
         self.conn = sqlite3.connect("database.db")
         self.cursor = conn.cursor()
-        
 
-    def insert_brewery(self,name,street,city,state/pronvince,) :
-        db.add()
+    def create_database(self,nom : str ,colonnes : list) :
         
+        # on convertie les noms de colonne sous forme d'énumération de str
+        noms_colonnes = str(colonnes[0])
+        for c in colonnes[1] : # on initialise à 1 pour l'ajout des virgules
+            noms_colonnes += ","+str(c)
         
+        # on crée le nom et les colonnes de la table
+        self.cursor.execute("CREATE TABLE "+nom+"(noms_colonnes)")  
+
+        pass
+
+
+
+    def insert_line(self,infos : list) : # on insère les informations d'une ligne sous forme de liste de valeurs
+        
+        # on convertie les informations sous forme d'énumération de str
+        infos = str(infos[0])
+        for valeur in infos[1] : # on initialise à 1 pour l'ajout des virgules
+            infos += ","+str(valeur)
+
+        self.cursor.execute("""INSERT INTO movie VALUES
+        ("""+infos+""")
+        """)
         
         pass
 
