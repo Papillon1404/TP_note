50a7f05953953a5e300551fc4a724c396469bc12
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2026-01-21 15:19:40 +0100

    line avec db

--- examen-webscrapping-/src/scrapper.py
+++ examen-webscrapping-/src/scrapper.py
@@ -24,4 +24,11 @@ noms = [D["name"] for D in html]
 villes = [D["city"] for D in html]
 types = [D["brewery_type"] for D in html]
 
-db.insert_
+db.creer_table(['Nom de la brasserie', 'Ville', 'Type'])
+
+for i in range(len(noms)) :
+    nom = noms[i]
+    ville = villes[i]
+    type = types[i]
+    db.inserer_ligne(nom, ville, type)
+
