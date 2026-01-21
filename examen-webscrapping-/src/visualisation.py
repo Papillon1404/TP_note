ef97913719ce59287698ba5d22ae9f219ff0b704
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2026-01-21 16:34:52 +0100

    visualisation début

--- examen-webscrapping-/src/visualisation.py
+++ examen-webscrapping-/src/visualisation.py
@@ -1,1 +1,14 @@
 import matplotlib as plt
+from analyse import *
+
+df  = read_table('Brasseries du Montana')
+
+# affichage des brasseries par type
+
+plt.figure(1)
+X = 
+Y = nb_brasseries_par_type(df)
+plt.plot(X,Y)
+plt.xlabel('Type de brasserie')
+plt.ylabel('Nombre')
+
