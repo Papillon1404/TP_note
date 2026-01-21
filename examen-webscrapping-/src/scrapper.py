86fb9e8008378d584c6a0481fd42cb7a253614b5
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2026-01-21 15:51:33 +0100

    lien avec db

--- examen-webscrapping-/src/scrapper.py
+++ examen-webscrapping-/src/scrapper.py
@@ -4,9 +4,9 @@ import json
 import sqlite3
 from database import DatabaseManager
 
-conn = sqlite3.connect("database.db")
+conn = sqlite3.connect("nom_bdd.db")
 cursor = conn.cursor()
-db = DatabaseManager("database.db")
+db = DatabaseManager("nom_bdd.db")
 
 url = "https://api.openbrewerydb.org/v1/breweries/search?query=Montana"
 params = {"per_page": 200} # on fait en sorte d'avoir tous les résultats (sinon on ne récupère pas toutes les brasseries)
@@ -24,11 +24,11 @@ noms = [D["name"] for D in html]
 villes = [D["city"] for D in html]
 types = [D["brewery_type"] for D in html]
 
-db.creer_table(['Nom de la brasserie', 'Ville', 'Type'])
+db.creer_table('Brasseries du Montana', ['Nom de la brasserie', 'Ville', 'Type'])
 
 for i in range(len(noms)) :
     nom = noms[i]
     ville = villes[i]
     type = types[i]
-    db.inserer_ligne(nom, ville, type)
+    db.inserer_ligne(nom, [ville, type])
 
